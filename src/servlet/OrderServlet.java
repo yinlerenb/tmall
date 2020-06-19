976676c6ed2a901af5dbf5f68f6cdabba2d2a306
yinlerenb <the.nexus2333@gmail.com>
2020-06-20 00:34:20 +0800

    test

--- src/servlet/OrderServlet.java
+++ src/servlet/OrderServlet.java
@@ -1,19 +1,56 @@
 package servlet;
 
+import bean.Order;
+import dao.OrderDAO;
+import util.Page;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @WebServlet(name = "OrderServlet")
-public class OrderServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class OrderServlet extends BaseBackServlet {
 
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
 
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Order o = orderDAO.get(id);
+        o.setDeliveryDate(new Date());
+        o.setStatus(OrderDAO.waitConfirm);
+        orderDAO.update(o);
+        return "@admin_order_list";
+    }
+
+    public String delivery(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Order o = orderDAO.get(id);
+        o.setDeliveryDate(new Date());
+        o.setStatus(OrderDAO.waitConfirm);
+        orderDAO.update(o);
+        return "@admin_order_list";
     }
 }

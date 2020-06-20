2abd78dc0dd8385911ea75da33d0467bb943385d
yinlerenb <the.nexus2333@gmail.com>
2020-06-20 08:47:55 +0800

    test

--- src/servlet/ProductServlet.java
+++ src/servlet/ProductServlet.java
@@ -1,19 +1,109 @@
 package servlet;
 
+import bean.Category;
+import bean.Product;
+import bean.PropertyValue;
+import util.Page;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet")
-public class ProductServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class ProductServlet extends BaseBackServlet {
 
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int cid = Integer.parseInt(request.getParameter("cid"));
+        Category c = categoryDAO.get(cid);
+        String name = request.getParameter("name");
+        String subTitle = request.getParameter("subTitle");
+        float originalPrice = Float.parseFloat(request.getParameter("originalPrice"));
+        float promotePrice = Float.parseFloat(request.getParameter("promotePrice"));
+        int stock = Integer.parseInt(request.getParameter("stock"));
+        Product p = new Product();
+        p.setCategory(c);
+        p.setName(name);
+        p.setSubTitle(subTitle);
+        p.setOriginalPrice(originalPrice);
+        p.setPromotePrice(promotePrice);
+        p.setStock(stock);
+        productDAO.add(p);
+        return "@admin_product_list?cid=" + cid;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product p = productDAO.get(id);
+        productDAO.delete(id);
+        return "@admin_product_list?cid=" + p.getCategory().getId();
+    }
 
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product p = productDAO.get(id);
+        request.setAttribute("p", p);
+        return "admin/editProduct.jsp";
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int cid = Integer.parseInt(request.getParameter("cid"));
+        Category c = categoryDAO.get(cid);
+        int id = Integer.parseInt(request.getParameter("id"));
+        int stock = Integer.parseInt(request.getParameter("stock "));
+        float originalPrice = Float.parseFloat(request.getParameter("originalPrice"));
+        float promotePrice = Float.parseFloat(request.getParameter("promotePrice"));
+        String subTitle = request.getParameter("subTitle");
+        String name = request.getParameter("name");
+        Product p = new Product();
+        p.setName(name);
+        p.setSubTitle(subTitle);
+        p.setOriginalPrice(originalPrice);
+        p.setPromotePrice(promotePrice);
+        p.setStock(stock);
+        p.setId(id);
+        p.setCategory(c);
+        productDAO.update(p);
+        return "@admin_product_list?cid=" + p.getCategory().getId();
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int cid = Integer.parseInt(request.getParameter("cid"));
+        Category c = categoryDAO.get(cid);
+        List<Product> ps = productDAO.list(cid, page.getStart(), page.getCount());
+        int total = productDAO.getTotal(cid);
+        page.setTotal(total);
+        page.setParam("&cid=" + c.getId());
+        request.setAttribute("ps", ps);
+        request.setAttribute("a", c);
+        request.setAttribute("page", page);
+        return "admin/listProduct.jsp";
+    }
+
+    public String editPropertyValue(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product p = productDAO.get(id);
+        request.setAttribute("p", p);
+        propertyValueDAO.init(p);
+        List<PropertyValue> pvs = propertyValueDAO.list(p.getId());
+        request.setAttribute("pvs", pvs);
+        return "admin/editProductValue.jsp";
+    }
+
+    public String updatePropertyValue(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int pvid = Integer.parseInt(request.getParameter("pvid"));
+        String value = request.getParameter("value");
+        PropertyValue pv = propertyValueDAO.get(pvid);
+        pv.setValue(value);
+        propertyValueDAO.update(pv);
+        return "%success";
     }
 }
--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -1,19 +1,46 @@
 package servlet;
 
+import bean.User;
+import util.Page;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "UserServlet")
-public class UserServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class UserServlet extends BaseBackServlet {
 
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
 
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        List<User> us = userDAO.list(page.getStart(), page.getCount());
+        int total = userDAO.getTotal();
+        page.setTotal(total);
+        request.setAttribute("us", us);
+        request.setAttribute("page", page);
+        return "admin/listUser.jsp";
     }
 }

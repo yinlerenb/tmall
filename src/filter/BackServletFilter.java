752266617edaa8e633fe9b17960c71476e96f9eb
yinlerenb <the.nexus2333@gmail.com>
2020-06-25 15:38:46 +0800

    test

--- src/filter/BackServletFilter.java
+++ src/filter/BackServletFilter.java
@@ -14,20 +14,28 @@ public class BackServletFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        HttpServletResponse response=(HttpServletResponse) resp;
-        HttpServletRequest request=(HttpServletRequest) req;
-        String contextPath=request.getServletContext().getContextPath();
-        //截取url的最后部分
-        String uri=request.getRequestURI();
-        uri= StringUtils.remove(uri,contextPath);
-        if (uri.startsWith("/admin_")){
-            String servletPath =StringUtils.substringBetween(uri,"_","_")+"Servlet";
-            String method=StringUtils.substringAfterLast(uri,"_");
-            request.setAttribute("method",method);
+        //req,resp类型转换
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        //获取项目根路径
+        String contextPath = request.getServletContext().getContextPath();
+        //获取uri的最后部分
+        String uri = request.getRequestURI();
+        uri = StringUtils.remove(uri, contextPath);
+        //判断是否为后台请求
+        if (uri.startsWith("/admin_")) {
+            //获取对应的servlet名称
+            String servletPath = StringUtils.substringBetween(uri, "_", "_") + "Servlet";
+            //获取对应的方法
+            String method = StringUtils.substringAfterLast(uri, "_");
+            //将方法设为属性传递
+            request.setAttribute("method", method);
+            //转发到对应的servlet
+            request.getRequestDispatcher("/" + servletPath).forward(request, response);
             return;
         }
         //请求放行
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
     }
 
     public void init(FilterConfig config) throws ServletException {

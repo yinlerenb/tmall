4664871c3feeed3e6d6ad27f101417ce1caab44f
yinlerenb <the.nexus2333@gmail.com>
2020-06-26 14:24:30 +0800

    test

--- src/servlet/BaseBackServlet.java
+++ src/servlet/BaseBackServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import dao.*;
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.omg.PortableInterceptor.INACTIVE;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -23,73 +25,81 @@ import java.util.Map;
 public abstract class BaseBackServlet extends HttpServlet {
 
     public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+
     public abstract String delete(HttpServletRequest request, HttpServletResponse response, Page page);
+
     public abstract String edit(HttpServletRequest request, HttpServletResponse response, Page page);
+
     public abstract String update(HttpServletRequest request, HttpServletResponse response, Page page);
+
     public abstract String list(HttpServletRequest request, HttpServletResponse response, Page page);
-    protected CategoryDAO categoryDAO=new CategoryDAO();
-    protected OrderDAO orderDAO=new OrderDAO();
-    protected OrderItemDAO orderItemDAO=new OrderItemDAO();
-    protected ProductDAO productDAO=new ProductDAO();
-    protected ProductImageDAO productImageDAO=new ProductImageDAO();
-    protected PropertyDAO propertyDAO=new PropertyDAO();
-    protected PropertyValueDAO propertyValueDAO=new PropertyValueDAO();
-    protected ReviewDAO reviewDAO=new ReviewDAO();
-    protected UserDAO userDAO=new UserDAO();
+
+    protected CategoryDAO categoryDAO = new CategoryDAO();
+    protected OrderDAO orderDAO = new OrderDAO();
+    protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+    protected ProductDAO productDAO = new ProductDAO();
+    protected ProductImageDAO productImageDAO = new ProductImageDAO();
+    protected PropertyDAO propertyDAO = new PropertyDAO();
+    protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+    protected ReviewDAO reviewDAO = new ReviewDAO();
+    protected UserDAO userDAO = new UserDAO();
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             //分页
-            int start=0;
-            int count=5;
+            int start = 0;
+            int count = 5;
             try {
-                start=Integer.parseInt(req.getParameter("page.start"));
-                count=Integer.parseInt(req.getParameter("page.count"));
+                start = Integer.parseInt(req.getParameter("page.start"));
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
-            Page page=new Page(start,count);
-
+            try {
+                count = Integer.parseInt(req.getParameter("page.count"));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            Page page = new Page(start, count);
             //借助反射调用对应的方法
-            String method=(String) req.getAttribute("method");
-            Method m=this.getClass().getMethod(method,HttpServletRequest.class,HttpServletResponse.class, Page.class);
-            String redirect=m.invoke(this,req,resp,page).toString();
-
-            //根据方法的返回值
-            if(redirect.startsWith("@")){
+            String method = (String) req.getAttribute("method");
+            Method m = this.getClass().getMethod(method, HttpServletRequest.class, HttpServletResponse.class, Page.class);
+            String redirect = m.invoke(this, req, resp, page).toString();
+            //根据方法的返回值执行对应的操作
+            if (redirect.startsWith("@")) {
                 resp.sendRedirect(redirect.substring(1));
-            }else if(redirect.startsWith("%")){
+            } else if (redirect.startsWith("%")) {
                 resp.getWriter().print(redirect.substring(1));
-            }else
-                req.getRequestDispatcher(redirect).forward(req,resp);
+            } else
+                req.getRequestDispatcher(redirect).forward(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    public InputStream parseUpload(HttpServletRequest request, Map<String, String> params){
-        InputStream is=null;
+    public InputStream parseUpload(HttpServletRequest request, Map<String, String> params) {
+        InputStream is = null;
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        //设置上传文件的大小
+        factory.setSizeThreshold(1024 * 10240);
         try {
-            DiskFileItemFactory factory=new DiskFileItemFactory();
-            ServletFileUpload upload=new ServletFileUpload(factory);
-            factory.setSizeThreshold(1024*10240);
-            List items=upload.parseRequest(request);
-            Iterator iter=items.iterator();
-            while (iter.hasNext()){
-                FileItem item=(FileItem) iter.next();
-                if (!item.isFormField()) {
-                    is=item.getInputStream();
-                }else {
-                    String paramName=item.getFieldName();
-                    String paramValue=item.getString();
-                    paramValue= new String(paramValue.getBytes("ISO-8859-1"),"UTF-8");
-                    params.put(paramName,paramValue);
+            List items = upload.parseRequest(request);
+            Iterator iter = items.iterator();
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+                if (item.isFormField()) {
+                    //获取上传文件的输入流
+                    is = item.getInputStream();
+                } else {
+                    String paramName = item.getFieldName();
+                    String paramValue = item.getString();
+                    paramValue = new String(paramValue.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+                    params.put(paramName, paramValue);
                 }
             }
-
-        } catch (Exception e) {
+        } catch (FileUploadException | IOException e) {
             e.printStackTrace();
         }
         return is;
